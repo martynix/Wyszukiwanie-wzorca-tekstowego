0f322ebd8b64c563f9abba0673a324a493cb8bfa
martynix <martyna410a@gmail.com>
2024-01-17 13:14:25 +0100

    3

--- C#/ConsoleApp1/Program.cs
+++ C#/ConsoleApp1/Program.cs
@@ -1,30 +1,21 @@
 ﻿class WyszukiwanieBaza
 {
-    protected string wzorzec;
+    protected string wzorzec;  // Protected - dostęp jest ograniczony do zawierającej klasy lub typów pochodzących z zawierającej klasy.
     protected string test;
 
-    public virtual void PodajWzorzec()
+    public void PodajWzorzec()  // Public - dostęp nieograniczony, kod jest dostępny dla wszystkich klas
     {
         Console.Write("Podaj wzorzec: ");
         wzorzec = Console.ReadLine();
     }
 
-    public virtual void PodajTest()
+    public void PodajTest()
     {
         Console.Write("Podaj test: ");
         test = Console.ReadLine();
     }
 
-    public virtual void DodajPrefiks()
-    {
-        Console.Write("Podaj prefiks: ");
-        string prefiks = Console.ReadLine();
-        test = prefiks + test;
-
-        Console.WriteLine($"Zmodyfikowane słowo: {test}");
-    }
-
-    public virtual void SprawdzDlugoscTestu()
+    public void SprawdzDlugoscTestu()
     {
         while (wzorzec.Length > test.Length)
         {
@@ -34,7 +25,7 @@
         }
     }
 
-    public virtual void WyszukajWzorzec()
+    public void WyszukajWzorzec()
     {
         int pozycja = test.IndexOf(wzorzec, StringComparison.OrdinalIgnoreCase);
 
@@ -42,18 +33,29 @@
         {
             Console.WriteLine("Nie znaleziono wzorca w tekście");
             Console.WriteLine();
-
         }
         else
         {
             do
             {
-                Console.WriteLine($"Wzorzec występuje na pozycji: {pozycja+1}");
+                Console.WriteLine($"Wzorzec występuje na pozycji: {pozycja + 1}");
                 pozycja = test.IndexOf(wzorzec, pozycja + 1, StringComparison.OrdinalIgnoreCase);
             } while (pozycja != -1);
             Console.WriteLine();
-
         }
+    }
+}
+
+class WyszukiwanieZPrefiksem : WyszukiwanieBaza
+{
+    public new void DodajPrefiks()
+    {
+        Console.Write("Podaj prefiks: ");
+        string prefiks = Console.ReadLine();
+        test = prefiks + test;
+
+        Console.WriteLine($"Zmodyfikowane słowo: {test}");
+        SprawdzDlugoscTestu(); // Dodane sprawdzenie długości po dodaniu prefiksu
     }
 }
 
@@ -61,14 +63,13 @@ class Program
 {
     static void Main()
     {
-        WyszukiwanieBaza wyszukiwanie = new WyszukiwanieBaza();
+        WyszukiwanieZPrefiksem wyszukiwanie = new WyszukiwanieZPrefiksem();
 
         while (true)
         {
             wyszukiwanie.PodajWzorzec();
             wyszukiwanie.PodajTest();
-            wyszukiwanie.DodajPrefiks();
-            wyszukiwanie.SprawdzDlugoscTestu();
+            wyszukiwanie.DodajPrefiks(); // Używamy nadpisanej metody z dziedziczenia
             wyszukiwanie.WyszukajWzorzec();
 
             Console.WriteLine("Naciśnij Enter, aby kontynuować...");
